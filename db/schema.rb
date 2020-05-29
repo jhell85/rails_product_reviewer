6ff59a768bcdc175ebc7fa8498d0f00e61744fac
jhell85 <jhell85@gmail.com>
2020-05-29 13:33:18 -0700

    add foreign key migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_182242) do
+ActiveRecord::Schema.define(version: 2020_05_29_191239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,8 +27,10 @@ ActiveRecord::Schema.define(version: 2020_05_29_182242) do
     t.string "author"
     t.text "content_body"
     t.integer "rating"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reviews", "products"
 end
