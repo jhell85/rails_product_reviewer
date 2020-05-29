a3f5b80668ff82d10dbe383df8fe5dd981b41122
jhell85 <jhell85@gmail.com>
2020-05-29 13:03:39 -0700

    add migration for create reviews

--- db/migrate/20200529182242_create_reviews.rb
+++ db/migrate/20200529182242_create_reviews.rb
@@ -4,6 +4,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
       t.string :author
       t.text :content_body
       t.integer :rating
+      t.column(:product_id, :integer)
 
       t.timestamps
     end
