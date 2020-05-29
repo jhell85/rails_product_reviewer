a04df8477964970fb1cfd53cd0c9c5d04db2c4ed
jhell85 <jhell85@gmail.com>
2020-05-29 16:51:27 -0700

    add product layout form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,7 +13,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to product_path
+      redirect_to products_path
     else 
       render :new
     end
@@ -41,7 +41,7 @@ class ProductsController < ApplicationController
   def destroy 
     @product = Product.find(params[:id])
     @product.destroy
-    redirect_to albums_path
+    redirect_to products_path
   end
 
 
