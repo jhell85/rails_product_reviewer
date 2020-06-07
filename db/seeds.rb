374b1719ee95351f3a7112ef408b99f9f724a412
jhell85 <jhell85@gmail.com>
2020-06-06 18:16:29 -0700

    add faker gem and seeded DB with products and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+50.times do
+  product = Product.create!(
+    name: Faker::Construction.material,
+    country_of_orgin: Faker::Coffee.origin,
+    cost: Faker::Number.within(range: 20..1000)
+  )
+  if product.persisted?
+    rand(10..30).times do
+      product.reviews.create!(
+        author: Faker::Superhero.name,
+        content_body: Faker::Lorem.paragraph_by_chars(number: 200, supplemental: false ),
+        rating: rand(1..5)
+      )
+    end
+  end
+end
+
+p "created #{Product.count} products & #{Review.count} reviews"
